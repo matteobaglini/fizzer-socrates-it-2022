b8288baa4696d30d28284530d28396e644291f90
Matteo Baglini <matteo.baglini@gmail.com>
2022-09-26 15:14:52 +0200

    add more comments

--- src/Core.hs
+++ src/Core.hs
@@ -14,6 +14,9 @@ socrates = "SoCraTes is awesome!!"
 -- suggested by @arialdomartini
 n // i = n `mod` i == 0
 
+-- Semigroup (<>)
+-- https://hackage.haskell.org/package/base-4.17.0.0/docs/Prelude.html#t:Semigroup
+
 -- Semigroup implementation for String (alias for [Char])
 -- https://hackage.haskell.org/package/base-4.17.0.0/docs/src/GHC.Base.html#line-301
 -- instance Semigroup [a] where
@@ -26,6 +29,15 @@ n // i = n `mod` i == 0
 --     a       <> Nothing = a
 --     Just a  <> Just b  = Just (a <> b)
 
+-- fromMaybe
+-- https://hackage.haskell.org/package/base-4.17.0.0/docs/Data-Maybe.html#v:fromMaybe
+
+-- fold
+-- https://hackage.haskell.org/package/base-4.17.0.0/docs/Data-Foldable.html#v:fold
+
+-- Alternative (<|>)
+-- https://hackage.haskell.org/package/base-4.17.0.0/docs/Control-Applicative.html#t:Alternative
+
 fizzer :: Int -> String
 fizzer n = fromMaybe number $ fold rules
   where
@@ -36,6 +48,12 @@ fizzer n = fromMaybe number $ fold rules
     number = show n
     whenDivBy word divisor = if n // divisor then Just word else Nothing
     whenContains word value = if show value `isInfixOf` show n then Just word else Nothing
+
+-- intercalate
+-- https://hackage.haskell.org/package/base-4.17.0.0/docs/Data-List.html#v:intercalate
+
+-- map
+-- https://hackage.haskell.org/package/base-4.17.0.0/docs/Prelude.html#v:map
 
 fizzerList :: [Int] -> String
 fizzerList = intercalate ", " . map fizzer
