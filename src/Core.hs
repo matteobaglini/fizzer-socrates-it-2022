49acbd6bdc86517fb4f91778b652681b9fd7473b
Matteo Baglini <matteo.baglini@gmail.com>
2022-09-26 14:52:13 +0200

    add comments abount semigroups

--- src/Core.hs
+++ src/Core.hs
@@ -10,6 +10,18 @@ socrates = "SoCraTes is awesome!!"
 -- suggested by @arialdomartini
 n // i = n `mod` i == 0
 
+-- Semigroup implementation for String (alias for [Char])
+-- https://hackage.haskell.org/package/base-4.17.0.0/docs/src/GHC.Base.html#line-301
+-- instance Semigroup [a] where
+--         (<>) = (++)
+
+-- Semigroup implementation for Maybe a
+-- https://hackage.haskell.org/package/base-4.17.0.0/docs/src/GHC.Base.html#line-428
+-- instance Semigroup a => Semigroup (Maybe a) where
+--     Nothing <> b       = b
+--     a       <> Nothing = a
+--     Just a  <> Just b  = Just (a <> b)
+
 fizzer :: Int -> String
 fizzer n = fromMaybe number $ fizz <> buzz
   where
